cdb56a47041f3e56dec153fe74f07e5365c219b9
simingg <53991172+simingg@users.noreply.github.com>
2021-02-08 11:41:58 +0800

    Add files via upload

---
+++ src/components/QuantityCounter.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button v-on:click="add" id="left"> + </button>
+         <p> {{ counter }} </p>
+        <button v-on:click="minus" id="right"> - </button>
+    </div>
+</template>
+
+<script>
+ export default {
+     name: 'QuantityCounter',
+     props : {
+         itemSelected: {
+             type: Object
+         }
+     },
+     data() {
+         return {
+             counter: 0,
+         }
+     },
+     methods: {
+        //creates a custome event called counter on this vue, passing itemSelected and counter.
+        add: function() {
+            this.counter = this.counter + 1;
+            this.$emit('counter', this.itemSelected, this.counter)
+        },
+        minus: function() {
+            this.counter = this.counter - 1;
+            this.$emit('counter', this.itemSelected, this.counter)
+        }
+     }
+ }
+</script>
+
+<style scoped>
+ button {
+    color:red;
+    size: 20px;
+    background-color: white;
+}
+</style>
