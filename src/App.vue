5cd0e0f24f48d9a2ec8d972ca53709bbed43daad
simingg <53991172+simingg@users.noreply.github.com>
2021-03-03 12:06:56 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1> Thai Food </h1>
+    <app-header> </app-header>
+    <router-view>
+    </router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+  name: 'App',
+  components: {
+     'app-header':Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
